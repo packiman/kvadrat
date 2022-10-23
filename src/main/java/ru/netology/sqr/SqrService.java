acd4a321933b63b14f4f7a1a3f560357ebc219a4
EvgenyKlishin <klishinev@gmail.com>
2022-10-23 12:27:02 +0300

    second

--- src/main/java/ru/netology/sqr/SqrService.java
+++ src/main/java/ru/netology/sqr/SqrService.java
@@ -5,7 +5,7 @@ public class SqrService {
     public int calcSqr(int start, int finish) {
         int variable = 0;
         for (int i = 10; i <= 99; i++) {
-            if (i * i >= start) {
+            if (i * i >= start & i * i <= finish) {
                 variable++;
             }
         }
