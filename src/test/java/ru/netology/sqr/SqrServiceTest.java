746a8928a7afcaafc55bfb7f16bff5a14e027f8a
EvgenyKlishin <klishinev@gmail.com>
2022-10-27 15:30:30 +0300

    code change

--- src/test/java/ru/netology/sqr/SqrServiceTest.java
+++ src/test/java/ru/netology/sqr/SqrServiceTest.java
@@ -8,19 +8,14 @@ import org.junit.jupiter.api.Test;
 
 
 public class SqrServiceTest {
-    @ParameterizedTest
-    // @CsvSource({
-    //"3, 200, 300 , позитивный тест"
-    //"6 ,200, 300 , негативный тест"
-    //"10 , 200, 600 , позитивный тест с другим диапозоном"
-    //"11, 200, 600 , негативный тест с другим диапозоном"
-    //  })
-    @CsvFileSource(files = "src/test/resources/numbersRange.csv")
-    public void calcSqrPos(int expected, int start, int finish) {
+
+    @Test
+    public void calcSqrPos() {
 
         SqrService service = new SqrService();
-        // int expected = 3;
-        int actual = service.calcSqr(start, finish);
+
+        int expected = 3;
+        int actual = service.calcSqr(200, 300);
 
         Assertions.assertEquals(expected, actual);
     }
